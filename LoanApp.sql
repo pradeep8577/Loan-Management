370bb60f71637227353b35003cb697815166b2c9
Pradeep Pawar <72540482+pradeep8577@users.noreply.github.com>
2023-11-23 16:37:21 +0530

    Update LoanApp.sql

--- LoanApp.sql
+++ LoanApp.sql
@@ -108,3 +108,9 @@ COLLATE = utf8mb4_0900_ai_ci;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- ----------------------------------------------------------------------
+-- To insert one admin into the admin table based on the schema you provided
+-- ----------------------------------------------------------------------  
+INSERT INTO `loanapp`.`admin` (`admin_id`, `a_email`, `a_password`, `fname`, `lname`)
+VALUES ('admin123', 'admin@example.com', 'adminpassword', 'John', 'Doe');
